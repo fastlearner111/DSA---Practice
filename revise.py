1e7ab0c53e1049b9272a9d93a0ed2cd0b0c01a65
fastlearner111 <sanjojdahal1@gmail.com>
2026-05-25 05:21:09 -0500

    Day 13: sliding window problems

--- revise.py
+++ revise.py
@@ -1,6 +1,14 @@
 nums = [2, 7, 11, 15] 
 target = 9
 
+#since we are looking for the index we are gonna use the index and number loop
+# so first we create a empty dict with a variable name seen
+# then we use the for loop
+#inside that we are gonna find the complment
+# then we are gonna see if that complement is in seen or not
+# if yes then we return the the complement number and index
+# else we update the index number and move to the next one and return nothing
+
 def two_sum(nums,target):
  seen = {}
 
