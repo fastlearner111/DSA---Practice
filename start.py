6cf1ecb163b5f737e9333907520f52c4682f2dfd
fastlearner111 <sanjojdahal1@gmail.com>
2026-06-15 03:24:26 -0500

    Day 34: Fundamentals drill 19/20, Reverse Linked List deep drill - logic understood, syntax still shaky

---
+++ start.py
@@ -0,0 +1,53 @@
+#Given the head of a singly linked list, reverse the list
+#and return the reversed list's head.
+
+head = [1,2,3,4,5]
+#Output: [5,4,3,2,1]
+#
+#Input:  head = [1,2]
+#Output: [2,1]
+#
+#Input:  head = []
+#Output: []
+
+# 1. Define the node class, 
+# # 2. Write your solution function,
+# # 3. Build test nodes, 
+# # 4. Call your function, 
+# # 5. Print results
+
+
+class LinkedList:
+    def __init__(self, val = 0, next = None):
+        self.val = val
+        self.next = next
+
+def reverselist(head):
+            prev = None
+            curr = head
+
+            while curr:
+                next = curr.next
+                curr.next = prev 
+                prev = curr
+                curr = next
+            return prev
+
+n1 = LinkedList(1)
+n2 = LinkedList(2)
+n3 = LinkedList(3)
+n4 = LinkedList(4)
+n5 = LinkedList(5)
+n1.next = n2
+n2.next =n3
+n3.next = n4
+n4.next =  n5
+
+head = reverselist(n1)
+
+
+current = head
+while current:
+      print(current.val, end = ">")
+      current = current.next
+        
