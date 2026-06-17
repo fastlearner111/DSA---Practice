65a2ae239bb55536dd9a42e4f2acc0f3c825ed41
fastlearner111 <sanjojdahal1@gmail.com>
2026-06-17 01:20:54 -0500

    Day 35: Fundamentals 20/20, Linked List Cycle, Tree traversals Preorder/Inorder/Postorder, Invert Binary Tree, Block 3 debugging

---
+++ invert_tree.py
@@ -0,0 +1,47 @@
+#Given the root of a binary tree, invert the tree
+#and return its root.
+#
+root = [4,2,7,1,3,6,9]
+##Output: [4,7,2,9,6,3,1]
+##
+##Input:  root = [2,1,3]
+#Output: [2,3,1]
+#
+#Input:  root = []
+#Output: []
+
+
+class TreeNode:
+    def __init__(self,val = 0, left = None, right = None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def invertTree(root):
+    if not root:
+        return None
+    root.left, root.right = root.right, root.left
+    invertTree(root.left)
+    invertTree(root.right)
+    return root
+
+root = TreeNode(4)
+root.left = TreeNode(2)
+root.right = TreeNode(7)
+root.left.left = TreeNode(1)
+root.left.right = TreeNode(3)
+root.right.left = TreeNode(6)
+root.right.right = TreeNode(9)
+
+def inorder(root):
+    if not root:
+        return []
+    return inorder(root.left) + [root.val] + inorder(root.right)
+
+result =invertTree(root)
+print(inorder(result))
+
+
+
+
+        
