9c1a7bdc4bdf00732445ca678f6fac0614665e47
fastlearner111 <sanjojdahal1@gmail.com>
2026-08-13 23:20:35 -0500

    new

---
+++ sliding_window.py/longest_substring_without_repeating.py
@@ -0,0 +1,18 @@
+s = "zxyzxyz"
+#Output: 3
+
+
+def longest(s):
+    left = 0
+    seen = set()
+    max_len = 0
+
+    for right in range(len(s)):
+        while s[right] in seen:
+            seen.remove(s[left])
+            left += 1
+
+        seen.add(s[right])
+        max_len = max(max_len, right - left + 1)
+    return max_len
+print(longest(s))
