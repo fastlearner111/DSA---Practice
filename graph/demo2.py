3b5aa6cf8b64b90001332f5bf7b0e38a3245bc47
fastlearner111 <sanjojdahal1@gmail.com>
2026-08-30 23:25:53 -0500

    course problem

---
+++ graph/demo2.py
@@ -0,0 +1,33 @@
+class Solution:
+    def canFinish(self, numCourses, prerequisities):
+
+        graph = {i: [] for i in range(numCourses)}
+        for a, b in prerequisities:
+            graph(b).append(a)
+
+        path = set()
+        visited = set()
+
+        def dfs(course):
+            if course in path:
+                return False
+            if course in visited:
+                return True
+
+            path.add(course)
+
+            for neighbor in graph[course]:
+                if not dfs(neighbor):
+                    return False
+
+            path.remove(course)
+            visited.add(course)
+
+            return True
+
+        for c in range(numCourses):
+            if not dfs(c):
+                return False
+
+        return True
+
